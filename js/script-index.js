16f8c36c9a3be414bfe6e3f2bd9a6745b70ab9f2
karemAlba <krmd91@gmail.com>
2016-12-04 22:11:54 +0000

    Etapa 1

--- js/script-index.js
+++ js/script-index.js
@@ -2,6 +2,7 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+	$(".icon-arrow-left-alt").hide();
 
 });
 
