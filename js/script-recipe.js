d135541f9d3826054d6f0bba38df202fdd17e3bc
karemAlba <krmd91@gmail.com>
2016-12-05 23:36:36 +0000

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	$(".icon-menu").hide();
 	console.log("Documento Listo");
-})
+
+	$('.js-show-recipe').click(function(){
+		$('.page').removeClass("make");
+	})
+	$('.js-show-make').click(function(){
+		$('.page').addClass("make");
+	})
+});
