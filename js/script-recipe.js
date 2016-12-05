3253f29254bf2a857cd4220d04df4dfb452066a2
karemAlba <krmd91@gmail.com>
2016-12-05 23:50:40 +0000

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,9 +3,15 @@ $(document).ready(function(){
 	console.log("Documento Listo");
 
 	$('.js-show-recipe').click(function(){
+		$(this).addClass("active");
 		$('.page').removeClass("make");
+		$('.js-show-make').removeClass("active");
+
 	})
 	$('.js-show-make').click(function(){
 		$('.page').addClass("make");
+		$(this).addClass("active");
+		$('.js-show-recipe').removeClass("active");
+		
 	})
 });
